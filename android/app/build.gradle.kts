422940105b46838349a1e010ff096878ce92be27
islandwaqwaq <islandwaqwaq@gmail.com>
2026-04-03 20:56:17 +0000

    release: version 1.1.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // >>>>>>>> ئەم دێڕە زیاد دەکرێت <<<<<<<<
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,4 +43,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// >>>>>>>> ئەم بەشە نوێیە زیاد دەکرێت <<<<<<<<
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
